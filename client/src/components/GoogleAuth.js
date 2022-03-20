63311f0dffc5b152ed9b174f07290c8e88f7d736
Akashdeep Chandrakar <63792903+akashchandrakar@users.noreply.github.com>
2022-03-20 23:58:56 +0530

    added signing fuction

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component{
+  state={isSignedIn:null}
   componentDidMount(){
     window.gapi.load('client:auth2',()=>{
       window.gapi.client.init({
         clientId:'166086071208-hukqh8jdoeegtg30l7dm71buoln6i60e.apps.googleusercontent.com',
         scope:'email'
+      }).then(()=>{
+        this.auth=window.gapi.auth2.getAuthInstance();
+        this.setState({isSignedIn:this.auth.isSignedIn.get()});
       });
     });
   }
+
+  renderButton(){
+    if(this.state.isSignedIn==null)
+       return <div>I don't know</div>;
+     else if(this.state.isSignedIn==null)
+        return <div>I am not signed in</div>;
+      else
+         return <div>I am not signed in</div>
+  }
   render(){
-    return <div>Google Auth</div>;
+    return <div>{this.renderButton()}</div>;
   }
 }
 
