8bc57f9a266f01c11465b76f18b4538bc934a59e
Akashdeep Chandrakar <63792903+akashchandrakar@users.noreply.github.com>
2022-03-21 17:47:46 +0530

    Added button of google authentication

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -10,17 +10,37 @@ class GoogleAuth extends React.Component{
       }).then(()=>{
         this.auth=window.gapi.auth2.getAuthInstance();
         this.setState({isSignedIn:this.auth.isSignedIn.get()});
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
+  onAuthChange=()=>{
+    this.setState({isSignedIn:this.auth.isSignedIn.get()});
+  };
 
+  onSignIn=()=>{
+     this.auth.signIn();
+  };
+  onSignOut=()=>{
+    this.auth.signOut();
+  };
   renderButton(){
-    if(this.state.isSignedIn==null)
-       return <div>I don't know</div>;
-     else if(this.state.isSignedIn==null)
-        return <div>I am not signed in</div>;
+    if(this.state.isSignedIn===null)
+       return null;
+     else if(this.state.isSignedIn)
+        return (
+          <button onClick={this.onSignOut} className="ui red google button">
+              <i className="google icon"/>
+              Sign Out
+          </button>
+        );
       else
-         return <div>I am not signed in</div>
+         return (
+           <button onClick={this.onSignIn} className="ui red google button">
+               <i className="google icon"/>
+               Sign In with google
+           </button>
+         );
   }
   render(){
     return <div>{this.renderButton()}</div>;
