2762825e66222b3f7fd554517bea21392359a5a2
Akashdeep Chandrakar <63792903+akashchandrakar@users.noreply.github.com>
2022-03-24 13:42:38 +0530

    Automatically form handling is wired up

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import {Field,reduxForm} from 'redux-form';
 
-const StreamCreate=()=>{
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component{
 
-export default StreamCreate;
+  renderInput(formProps){
+    console.log(formProps);
+    return <input {...formProps.input}/>
+  }
+  render(){
+        return (
+           <form>
+           <Field name="title" component={this.renderInput}/>
+           <Field name="description" component={this.renderInput}/>
+           </form>
+        );
+   }
+}
+
+export default reduxForm({form:'StreamCreate'})(StreamCreate);
