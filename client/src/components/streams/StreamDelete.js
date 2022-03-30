b743593d618882ad726f76464bc387f2f8cbb25f
Akashdeep Chandrakar <63792903+akashchandrakar@users.noreply.github.com>
2022-03-30 14:20:16 +0530

    Completed Modal pop up

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history'
 
 const StreamDelete=()=>{
+  const action=(
+    <React.Fragment>
+     <button className="ui button negative">Delete</button>
+     <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
   return (
     <div>
       StreamDelete
-      <Modal/>
+      <Modal
+       title="Delete Stream"
+       content="Are you sure you want to delete this stream?"
+       action={action}
+       onDismiss={()=>history.push('/')}
+      />
     </div>
   );
 };
